81297934eab01a0d443c98d1f9b230dae49dd77e
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-02-27 22:16:53 +0100

    Listen for service status changes using SSE
    This is more reliable and less noisy than polling.

--- ServiceStatus.tsx
+++ ServiceStatus.tsx
@@ -5,30 +5,18 @@ export default function ServiceStatus() {
   const [status, setStatus] = useState<'online' | 'offline'>('offline');
 
   useEffect(() => {
-    // Poll the service worker entry point (uncached) to see if it is live or stale
-    // TODO: Look into replacing this with SSE (will help with the SSH tunnel too)
-    const interval = setInterval(
-      async () => {
-        try {
-          const response = await fetch('/worker', { method: 'HEAD' });
-          if (!response.ok) {
-            throw new Error(`Service worker entry point ${response.status} ${response.statusText}`);
-          }
+    const abortController = new AbortController();
+    const eventSource = new EventSource('/status');
 
-          setStatus('online');
-        }
-        catch (error) {
-          if (error.message !== 'NetworkError when attempting to fetch resource.') {
-            throw error;
-          }
+    eventSource.addEventListener('open', () => setStatus('online'), { signal: abortController.signal });
+    eventSource.addEventListener('message', () => setStatus('online'), { signal: abortController.signal });
+    eventSource.addEventListener('error', () => setStatus('offline'), { signal: abortController.signal });
+    eventSource.addEventListener('close', () => setStatus('offline'), { signal: abortController.signal });
 
-          setStatus('offline');
-        }
-      },
-      1000
-    );
-
-    return () => clearInterval(interval);
+    return () => {
+      eventSource.close();
+      abortController.abort();
+    }
   }, []);
 
   return (
--- index.ts
+++ index.ts
@@ -8,6 +8,25 @@ Bun.serve({
     '/worker': worker,
   },
   fetch(request) {
+    const url = new URL(request.url);
+    if (url.pathname === '/status') {
+      return new Response(
+        async function* () {
+          while (!request.signal.aborted) {
+            yield `data: ${new Date().toISOString()}\n\n`;
+            await Bun.sleep(1000);
+          }
+        },
+        {
+          headers: {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
+          }
+        }
+      );
+    }
+
     return new Response('Hello, world!');
   }
 });
