a6fdd2dd82b82de0f3f713eb712794f123015c93
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-03-02 14:30:23 +0100

    Expand the Demo section to be a CRUD experience over basic items
    Right now this only works online and I will follow-up with work on the service worker to make this offline-friendly.

--- Demo.tsx
+++ Demo.tsx
@@ -1,61 +1,168 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Title from './Title';
 import Button from './Button';
+import trimUrl from './trimUrl';
 
-type BaseResult = { id: number; stamp: string; };
-type SuccessResult = BaseResult & { result: string; source: 'service-worker' | 'web-server'; };
-type ErrorResult = BaseResult & { error: string; };
+type Item = {
+  id: number;
+  name: string;
+};
+
+type Call = {
+  id: number;
+  stamp: string;
+  source: 'service-worker' | 'web-server';
+  url: URL;
+} & Pick<Response, 'ok' | 'status' | 'statusText'>;
 
 export default function Demo() {
-  const [results, setResults] = useState<(SuccessResult | ErrorResult)[]>([]);
-
-  const handleDemoButtonClick = useCallback(async () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [calls, setCalls] = useState<Call[]>([]);
+  
+  const trackCall = useCallback((response: Response) => {
     const stamp = new Date().toISOString().slice(0, 'yyyy-mm-dd-hh-mm-ss'.length).replace('T', ' ');
-
-    try {
-      const response = await fetch('/api/test');
-      const text = await response.text();
-      const source = response.headers.get('x-service-worker') ? 'service-worker' as const : 'web-server' as const;
-      setResults(results => [{ id: results.length, stamp, result: text, source }, ...results]);
-    }
-    catch (error) {
-      setResults(results => [{ id: results.length, stamp, error: error.message }, ...results]);
-    }
+    const { url, ok, status, statusText } = response;
+    const source = response.headers.get('x-service-worker') ? 'service-worker' as const : 'web-server' as const;
+    setCalls(calls => [{ id: calls.length, stamp, source, url: new URL(url), ok, status, statusText }, ...calls]);
   }, []);
 
-  const handleClearButtonClick = useCallback(() => {
-    setResults([]);
+  const fetchItems = useCallback(async () => {
+    const response = await fetch('/api/items');
+    setItems(await response.json());
+    trackCall(response);
+  }, [trackCall]);
+
+  useEffect(() => {
+    fetchItems()
   }, []);
+
+  const handleAddButtonClick = useCallback(async () => {
+    const name = prompt('New item name:');
+    if (!name) {
+      return;
+    }
+
+    const response = await fetch('/api/items', { method: 'POST', body: JSON.stringify({ name }) });
+    trackCall(response);
+    await fetchItems();
+  }, []);
+
+  const handleRenameButtonClick = useCallback(async (event: React.MouseEvent<HTMLButtonElement>) => {
+    const id = Number(event.currentTarget.dataset.id);
+    if (!id) {
+      throw new Error('ID not found.');
+    }
+
+    const item = items.find(item => item.id === id);
+    if (!item) {
+      throw new Error('Item not found.');
+    }
+
+    const name = prompt('New item name:', item.name);
+    if (!name) {
+      return;
+    }
+
+    const response = await fetch(`/api/items/${id}`, { method: 'PUT', body: JSON.stringify({ name }) });
+    trackCall(response);
+    await fetchItems();
+  }, [items]);
+
+  const handleDeleteButtonClick = useCallback(async (event: React.MouseEvent<HTMLButtonElement>) => {
+    const id = Number(event.currentTarget.dataset.id);
+    if (!id) {
+      throw new Error('ID not found.');
+    }
+
+    const item = items.find(item => item.id === id);
+    if (!item) {
+      throw new Error('Item not found.');
+    }
+
+    if (!confirm(`Are you sure you want to purge ${item.name}?`)) {
+      return;
+    }
+
+    const response = await fetch(`/api/items/${id}`, { method: 'DELETE' });
+    trackCall(response);
+    await fetchItems();
+  }, [items]);
+
+  const handleClearDatabaseButtonClick = useCallback(async () => {
+    if (!confirm('Are you sure you want to purge the database?')) {
+      return;
+    }
+
+    const response = await fetch('/api/items', { method: 'DELETE' });
+    trackCall(response);
+    await fetchItems();
+  }, []);
+
+  const handleClearCacheButtonClick = useCallback(() => {
+    setCalls([]);
+  }, []);
+
+  const idDigits = useMemo(() => ~~Math.log10(Math.max(...items.map(item => item.id))) + 1, [items]);
 
   return (
     <>
-      <Title text='Demo' />
+      <Title text='Data' />
       <div className='flex gap-1'>
-        <Button text='Demo' onClick={handleDemoButtonClick} />
-        {!!results.length && (
-          <Button text='Clear' onClick={handleClearButtonClick} danger />
-        )}
+        <Button text='Add' onClick={handleAddButtonClick} />
       </div>
-      {!results.length && (
-        <div className='text-slate-500'>
-          Hit the button with the service online and offline to see the difference.
-        </div>
-      )}
-      <ul>
-        {results.map(result => (
-          <li key={result.id}>
-            <time dateTime={result.stamp} className='text-slate-500'>{result.stamp}:</time>
-            {' '}
-            {'error' in result
-              ? result.error
-              : (
-                <>
-                  {result.result} <span className='text-slate-500'>({result.source})</span>
-                </>
-              )}
-          </li>
-        ))}
-      </ul>
+      {items.length
+        ? (
+          <div>
+            <div className='flex flex-col gap-1 mb-1'>
+              {items.map(item => (
+                <div key={item.id} className='flex gap-1 hover:bg-slate-100'>
+                  <code className='text-slate-500'>
+                    {item.id.toString().padStart(idDigits, '0')}
+                  </code>
+                  <span className='mr-auto'>
+                    {item.name}
+                  </span>
+                  <Button text='Rename' onClick={handleRenameButtonClick} data-id={item.id} />
+                  <Button text='Delete' onClick={handleDeleteButtonClick} danger data-id={item.id} />
+                </div>
+              ))}
+            </div>
+            <Button text='Clear' onClick={handleClearDatabaseButtonClick} danger />
+          </div>
+        )
+        : (
+          <div className='text-slate-500'>
+            The database is empty.
+          </div>
+        )
+      }
+      <Title text='Calls' />
+      {calls.length
+        ? (
+          <div>
+            <div className='flex flex-col gap-1 mb-1'>
+              {calls.map(call => (
+                <div key={call.id} className='flex gap-1 hover:bg-slate-100'>
+                  <time dateTime={call.stamp} className='text-slate-500 tabular-nums'>{call.stamp}:</time>
+                  <code className={call.ok ? 'text-green-500' : 'text-red-500'}>
+                    {call.status} {call.statusText}
+                  </code>
+                  <code>
+                    {trimUrl(call.url)}
+                  </code>
+                  <span className='ml-auto text-slate-500'>({call.source})</span>
+                </div>
+              ))}
+            </div>
+            <Button text='Clear' onClick={handleClearCacheButtonClick} danger />
+          </div>
+        )
+        : (
+          <div className='text-slate-500'>
+            Hit the button with the service online and offline to see the difference.
+          </div>
+        )
+      }
     </>
   );
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import Bun from 'bun';
+import { Database } from "bun:sqlite";
 import index from './index.html';
+
+const db = new Database("data.sqlite");
+db.run("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT)");
 
 Bun.serve({
   development: true,
@@ -14,7 +18,34 @@ Bun.serve({
 
       return new Response(await build.outputs[0].text(), { headers: { 'Content-Type': 'application/javascript' } });
     },
-    '/api/test': new Response('Hello, world!'),
+    '/api/items': {
+      GET: async () => {
+        const items = db.query("SELECT * FROM items").all();
+        return new Response(JSON.stringify(items), { headers: { 'Content-Type': 'application/json' } });
+      },
+      POST: async request => {
+        const { name } = await request.json();
+        const { lastInsertRowid } = db.run("INSERT INTO items (name) VALUES (?)", [name]);
+        return new Response(JSON.stringify({ id: lastInsertRowid, name }), { headers: { 'Content-Type': 'application/json' } });
+      },
+      DELETE: async () => {
+        db.run("DELETE FROM items");
+        return new Response(null, { status: 204 });
+      },
+    },
+    '/api/items/:id': {
+      PUT: async request => {
+        const id = Number(request.params.id);
+        const { name } = await request.json();
+        db.run("UPDATE items SET name = ? WHERE id = ?", [name, id]);
+        return new Response(null, { status: 204 });
+      },
+      DELETE: async request => {
+        const id = Number(request.params.id);
+        db.run("DELETE FROM items WHERE id = ?", [id]);
+        return new Response(null, { status: 204 });
+      },
+    },
     '/api/status': request => new Response(
       async function* () {
         while (!request.signal.aborted) {
